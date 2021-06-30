17ae9900bbf78604ea11a6cbe49b83b3af246d7d
Zachary Gaines <zacharygaines28@gmail.com>
2021-06-30 11:37:29 -0700

    auth controller

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,8 +3,11 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
+
+
 describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
 });
