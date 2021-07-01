c8d30a8e2fd2a6a3bc95229a4ed9fdbba81f575d
Zachary Gaines <zacharygaines28@gmail.com>
2021-07-01 10:46:41 -0700

    get all instagram posts

--- __tests__/gram.test.js
+++ __tests__/gram.test.js
@@ -2,19 +2,20 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
-
+import Gram from '../lib/models/Gram.js';
 
 
 
 describe('demo routes', () => {
+
   let agent;
-  beforeAll(() => {
+
+  beforeEach(() => {
     return setup(pool);
   });
+
   beforeEach(async () => {
-
     agent = request.agent(app);
-
     await agent
       .post('/api/v1/auth/signup')
       .send({
@@ -31,20 +32,43 @@ describe('demo routes', () => {
   });
 
 
+
+
   it('creates an instagram post', async () => {
     const res = await agent
       .post('/api/v1/grams')
       .send({
         photoUrl: 'https://www.placecage.com/c/200/300',
-        caption: 'This is my first instagram post',
+        caption: 'first instagram post',
         tags: 'placecage'
       });
     expect(res.body).toEqual({
       id: '1',
       userId: '1',
       photoUrl: 'https://www.placecage.com/c/200/300',
+      caption: 'first instagram post',
+      tags: 'placecage'
+    });
+  });
+
+
+  it('gets all instagram posts', async () => {
+    const gram1 = await Gram.insert({
+      photoUrl: 'https://www.placecage.com/c/200/300',
       caption: 'This is my first instagram post',
       tags: 'placecage'
     });
+    const gram2 = await Gram.insert({
+      photoUrl: 'https://www.placecage.com/c/400/500',
+      caption: 'This is my second instagram post',
+      tags: 'placecage2'
+    });
+    const gram3 = await Gram.insert({
+      photoUrl: 'https://www.placecage.com/c/500/600',
+      caption: 'This is my third instagram post',
+      tags: 'placecage3'
+    });
+    const res = await request(app).get('/api/v1/grams');
+    expect(res.body).toEqual([gram1, gram2, gram3]);
   });
 });
